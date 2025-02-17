c2dca3f9b6e9c65ae8433c36dd28b2dbe3913b80
aadigb <109623381+aadigb@users.noreply.github.com>
2025-02-16 20:06:36 -0800

    Delete public/src/omponents/ui directory

--- public/src/omponents/ui/card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children }) => {
-  return <div className="p-4 bg-white shadow-md rounded">{children}</div>;
-};
-
-export const CardContent = ({ children }) => {
-  return <div>{children}</div>;
-};
